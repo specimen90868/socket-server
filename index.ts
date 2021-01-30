0d61e5f9a1f303a2439d6c0c4a8078e04ca873a8
Fabricio Vilchis <fvilchis@financialsolutions.mx>
2021-01-30 13:30:03 -0600

    seccion 4

--- index.ts
+++ index.ts
@@ -1,16 +1,12 @@
 import Server from './classes/server';
 import router from './routes/router';
 import bodyParser from 'body-parser';
-import cors from 'cors'
 
-const server = new Server();
+const server = Server.instance;
 
 // Body Parser
 server.app.use(bodyParser.urlencoded({ extended: true }));
 server.app.use(bodyParser.json());
-
-//CORS
-server.app.use(cors({origin: true, credentials: true}))
 
 // Rutas de Servicios
 server.app.use('/', router);
---
+++ sockets/sockets.ts
@@ -0,0 +1,22 @@
+import { Socket } from 'socket.io' 
+import socketIO from 'socket.io';
+
+export const desconectar = (cliente: Socket) => {
+
+  cliente.on('disconnect', () => {
+    console.log('Cliente desconectado');
+  });
+
+}
+
+// Escuchar mensajes
+export const mensaje = (cliente: Socket, io: socketIO.Server) => {
+
+  cliente.on('mensaje', (payload) => {
+    console.log('Mensaje recibido:', payload);
+
+    io.emit('mensaje-nuevo', payload);;
+    
+  })
+
+}
